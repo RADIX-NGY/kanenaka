679c5c774bbc6d74a2b05bc8c8db1f2f4a849904
RADIX-NGY <68005519+RADIX-NGY@users.noreply.github.com>
2021-03-24 19:00:18 +0900

    Add files via upload

---
+++ js/start-animation.js
@@ -0,0 +1,8 @@
+$(function(){
+    setTimeout(function(){
+    $('.tobi-group').addClass("front");
+    },4800);
+    setTimeout(function(){
+    $('.animation-area').fadeOut("slow");
+    },7000);
+});
